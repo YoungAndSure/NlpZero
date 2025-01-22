97f6075694ebc6a63a26cb882d2b82e3f28403d9
YoungAndSure <yangshuorex@foxmail.com>
2025-01-22 13:58:38 +0800

    add co_matrix, cos_similary

--- common/util.py
+++ common/util.py
@@ -1,5 +1,7 @@
 #! python3
 
+import numpy as np
+
 def preprocess(text) :
   text = text.lower()
   text = text.replace('.', ' .')
@@ -16,3 +18,26 @@ def preprocess(text) :
   corpus = [word2id[w] for w in words]
 
   return corpus, word2id, id2word
+
+def create_co_matrix(corpus, vocab_size, window_size=1) :
+  corpus_size = len(corpus)
+  co_matrix = np.zeros((vocab_size, vocab_size)).astype(np.float32)
+
+  for index,id in enumerate(corpus) :
+    for i in range(1, window_size + 1) :
+      left_i = index - i
+      right_i = index + i
+
+      if left_i > 0 :
+        left_word_id = corpus[left_i]
+        co_matrix[id][left_word_id] += 1
+      if right_i < corpus_size :
+        right_word_id = corpus[right_i]
+        co_matrix[id][right_word_id] += 1
+  
+  return co_matrix
+
+def cos_similarity(x, y, eps=1e-8) :
+  x = x / np.sqrt(np.sum(x ** 2) + 1e-8)
+  y = y / np.sqrt(np.sum(y ** 2) + 1e-8)
+  return np.dot(x, y)
--- utest/util_test.py
+++ utest/util_test.py
@@ -15,4 +15,16 @@ class UtilTest(unittest.TestCase) :
     self.assertEqual(word_to_id, {'you': 0, 'say': 1, 'goodbye': 2, 'and': 3, 'i': 4, 'hello': 5, '.': 6})
     self.assertEqual(id_to_word, {0: 'you', 1: 'say', 2: 'goodbye', 3: 'and', 4: 'i', 5: 'hello', 6: '.'})
 
+  def test_co_matrix(self) :
+    text = 'You say goodbye and I say hello.'
+    corpus, word_to_id, id_to_word = preprocess(text)
+    co_matrix = create_co_matrix(corpus, len(id_to_word), window_size=1)
+    self.assertTrue(np.array_equal(co_matrix,[[0,1,0,0,0,0,0],[0,0,1,0,1,1,0],[0,1,0,1,0,0,0],[0,0,1,0,1,0,0],[0,1,0,1,0,0,0],[0,1,0,0,0,0,1],[0,0,0,0,0,1,0]]))
+  
+  def test_cos_similarity(self) :
+    x = np.array([1, 2, 3])
+    y = np.array([4, 5, 6])
+    z = cos_similarity(x, y)
+    self.assertTrue(np.allclose(z, 0.9746318457857057))
+
 unittest.main()
