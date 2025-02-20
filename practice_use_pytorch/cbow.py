d2284680a38bc6425b7e44b7c8b8f28a47a4d5fc
YoungAndSure <yangshuorex@foxmail.com>
2025-02-20 11:24:49 +0800

    fix loss, optimizer use adam

--- practice_use_pytorch/cbow.py
+++ practice_use_pytorch/cbow.py
@@ -25,16 +25,14 @@ for i in range(len(ori_targets)) :
 class CbowModel(nn.Module):
   def __init__(self, vocab_size, hidden_size):
     super().__init__()
-    self.l0 = nn.Embedding(vocab_size, hidden_size)
-    self.l1 = nn.Embedding(vocab_size, hidden_size)
+    self.in_emb = nn.Embedding(vocab_size, hidden_size)
+    self.out_emb = nn.Embedding(vocab_size, hidden_size)
     self.activator = nn.Sigmoid()
 
   def forward(self, contexts, t):
-    y = None
-    for context in contexts :
-      y = y + self.l0(context) if y is not None else self.l0(context)
-    y = y * self.l1(t)
-    y = self.activator(y)
+    con_emb = self.in_emb(contexts).sum(dim=1)
+    target_emb = self.out_emb(t).squeeze(1).T
+    y = con_emb.matmul(target_emb)
     return y
 
 vocab_size = len(word2id)
@@ -42,31 +40,32 @@ hidden_size = 3
 model = CbowModel(vocab_size, hidden_size)
 
 # 设置优化器
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 # 初始化负样本采样
 unigram_sampler = UnigramSampler(corpus, 0.75, 5)
 
 # 设置损失函数
-loss_fn = nn.CrossEntropyLoss()
+# 集成了sigmoid的二元交叉熵误差函数
+loss_fn = nn.BCEWithLogitsLoss()
 
 # 训练
-max_epoch = 100
+max_epoch = 10
 for epoch in range(max_epoch) :
   for i in range(len(ori_targets)) :
-    x = torch.from_numpy(np.array(ori_contexts[i])[:, np.newaxis])
-    positive = torch.from_numpy(np.array([ori_targets[i]]))
-    y = model.forward(x, positive)
-    loss = loss_fn(y, torch.ones(1, dtype=torch.long))
+    x = torch.from_numpy(np.array([ori_contexts[i]]))
+    pt = torch.from_numpy(np.array([[ori_targets[i]]]))
+    y = model.forward(x, pt)
+    loss = loss_fn(y, torch.ones((1, 1)))
 
     negatives = unigram_sampler.get_negative_sample(np.array([ori_targets[i]]))[0]
     for negative in negatives :
       nt = torch.from_numpy(np.array([negative], dtype=np.long))
       y = model.forward(x, nt)
-      loss += loss_fn(y, torch.zeros(1, dtype=torch.long))
+      loss += loss_fn(y, torch.zeros((1, 1)))
 
     loss.backward()
     # deone中的update
     optimizer.step()
     optimizer.zero_grad()
-    print("epoch:{}, loss:{}".format(epoch, loss.data))
+    print("epoch:{}, loss:{}".format(epoch, loss))
