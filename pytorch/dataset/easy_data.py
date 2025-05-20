edf4d6b97f7bce1ff04d640bbae8ce494fc5ff75
YoungAndSure <yangshuorex@foxmail.com>
2025-05-20 10:59:17 +0800

    support close unigram_sampler in EasyDataset

--- pytorch/dataset/easy_data.py
+++ pytorch/dataset/easy_data.py
@@ -3,23 +3,31 @@ from torch.utils.data import Dataset
 from common.util import *
 
 class EasyDataset(Dataset):
-    def __init__(self, window_size=1):
+    def __init__(self, window_size=1, enable_unigram_sampler=True):
       text = 'You say goodbye and I say hello.'
       self.corpus, word2id, id2word = preprocess(text)
-      self.contexts, self.targets = create_contexts_target(corpus=self.corpus, window_size=window_size)
+      contexts, targets = create_contexts_target(corpus=self.corpus, window_size=window_size)
+      self.contexts, self.targets = np.array(contexts), np.array(targets)
+      self.enable_unigram_sampler = enable_unigram_sampler
 
       # 初始化负样本采样
-      self.unigram_sampler = UnigramSampler(self.corpus, 0.75, 5)
+      if self.enable_unigram_sampler :
+        self.unigram_sampler = UnigramSampler(self.corpus, 0.75, 5)
     
     def __getitem__(self, index):
       input = self.contexts[index, :]
       positive_targets = self.targets[index, :]
-      negative_targets = self.unigram_sampler.get_negative_sample(positive_targets)
-      targets = np.concatenate((positive_targets[np.newaxis, :], negative_targets), axis=1)
-
       positive_labels = np.ones_like(positive_targets, dtype=np.float32)
-      negative_labels = np.zeros_like(negative_targets, dtype=np.float32)
-      labels = np.concatenate((positive_labels[np.newaxis, :], negative_labels), axis=1)
+
+      if self.enable_unigram_sampler :
+        negative_targets = self.unigram_sampler.get_negative_sample(positive_targets)
+        negative_labels = np.zeros_like(negative_targets, dtype=np.float32)
+        targets = np.concatenate((positive_targets[np.newaxis, :], negative_targets), axis=1)
+        labels = np.concatenate((positive_labels[np.newaxis, :], negative_labels), axis=1)
+      else :
+        targets = positive_targets
+        labels = positive_labels
+
       return input, targets.squeeze(), labels
     
     def __len__(self):
